447ec8e857655508e039699106449bf9917bd48a
Sxnty <sxntycsgo@gmail.com>
2023-02-19 23:20:42 -0300

    added structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Todo from "./components/Todo";
 function App() {
   return (
-    <>
-      <Todo />
-    </>
+    <main className="hero"> 
+            <Todo />
+    </main>
   );
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -13,33 +13,40 @@ function Todo() {
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (tasks.find((task) => task.title === newTask)) {
-            alert("Ya existe una tarea con este título.");
-          } else {
-            setTasks([...tasks, { title: newTask, status: false }]);
-          }
-        }}
-      >
-        <h1>Todo</h1>
-        <input
-          type="text"
-          placeholder="go to gym..."
-          onChange={onChangeHandle}
-        />
-      </form>
-      <TasksList
-        tasks={tasks}
-        setTasks={setTasks}
-        setCompleted={setCompleted}
-        completedTasks={completedTasks}
-      />
-      <CompletedTasks
-        completedTasks={completedTasks}
-        setCompleted={setCompleted}
-      />
+      <section className="hero__tasklist">
+        <div className="tasks__actives">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (tasks.find((task) => task.title === newTask)) {
+                alert("Ya existe una tarea con este título.");
+              } else {
+                setTasks([...tasks, { title: newTask, status: false }]);
+              }
+            }}
+          >
+            <h1>Todo</h1>
+            <input
+              type="text"
+              placeholder="go to gym..."
+              onChange={onChangeHandle}
+            />
+          </form>
+
+          <TasksList
+            tasks={tasks}
+            setTasks={setTasks}
+            setCompleted={setCompleted}
+            completedTasks={completedTasks}
+          />
+        </div>
+        <div className="tasks__completed">
+          <CompletedTasks
+            completedTasks={completedTasks}
+            setCompleted={setCompleted}
+          />
+        </div>
+      </section>
     </>
   );
 }
